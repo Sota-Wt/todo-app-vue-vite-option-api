b04046f2a5616882661f93f6a23888f67d5ccef6
sota-K <violscaerley6699@gmail.com>
2023-09-28 21:12:59 +0900

    Completed

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import PlayGround from '../views/PlayGround.vue';
+import TodoApp from '../views/TodoApp.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +23,11 @@ const router = createRouter({
       path: '/play-ground',
       name: 'play-ground',
       component: PlayGround
+    },
+    {
+      path: '/todo',
+      name: 'todo',
+      component: TodoApp
     }
   ]
 });
--- src/views/PlayGround.vue
+++ src/views/PlayGround.vue
@@ -1,14 +1,14 @@
 <script>
 // import { defineComponent } from 'vue'
-import Chutorial_2 from '../components/PlayGround/Chutorial_2.vue'
-import Chutorial_3 from '../components/PlayGround/Chutorial_3.vue'
-import Chutorial_4 from '../components/PlayGround/Chutorial_4.vue'
-import Chutorial_5 from '../components/PlayGround/Chutorial_5.vue'
-import Chutorial_6 from '../components/PlayGround/Chutorial_6.vue'
-import Chutorial_7 from '../components/PlayGround/Chutorial_7.vue'
-import Chutorial_8 from '../components/PlayGround/Chutorial_8.vue'
-import Chutorial_9 from '../components/PlayGround/Chutorial_9.vue'
-import Chutorial_10 from '../components/PlayGround/Chutorial_10.vue'
+import Chutorial_2 from '../components/PlayGround/Chutorial_2.vue';
+import Chutorial_3 from '../components/PlayGround/Chutorial_3.vue';
+import Chutorial_4 from '../components/PlayGround/Chutorial_4.vue';
+import Chutorial_5 from '../components/PlayGround/Chutorial_5.vue';
+import Chutorial_6 from '../components/PlayGround/Chutorial_6.vue';
+import Chutorial_7 from '../components/PlayGround/Chutorial_7.vue';
+import Chutorial_8 from '../components/PlayGround/Chutorial_8.vue';
+import Chutorial_9 from '../components/PlayGround/Chutorial_9.vue';
+import Chutorial_10 from '../components/PlayGround/Chutorial_10.vue';
 
 export default {
     components: {

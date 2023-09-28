30ba53b946b9e91ea5ac29e0315462299d934b07
sota-K <violscaerley6699@gmail.com>
2023-09-28 22:13:50 +0900

    AddFontAwesome

--- src/main.js
+++ src/main.js
@@ -14,6 +14,21 @@ import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+/* import specific icons */
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { faPen } from '@fortawesome/free-solid-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faTrashCan);
+library.add(faPen);
+library.add(faCheck);
+
 const vuetify = createVuetify({
     components,
     directives
@@ -22,5 +37,6 @@ const vuetify = createVuetify({
 app.use(createPinia());
 app.use(router);
 app.use(vuetify);
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.mount('#app');
